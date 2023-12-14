d0673a8c28414c57739764b29d2c1afa9c3d83ea
yunsungbin <dbstmdqls0211@gmail.com>
2023-12-14 11:21:01 +0900

    scripts-Node_Change

---
+++ Assets/Scripts/MyType.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyType : MonoBehaviour
+{
+    public enum Unit { Sword, Range, Guard, Wizard, Bullet };
+}
