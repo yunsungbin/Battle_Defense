24c0130f257d482f2f2e3d710ffda065a916b01a
yunsungbin <dbstmdqls0211@gmail.com>
2023-12-29 10:46:09 +0900

    scanAlly

--- Assets/Scripts/MyType.cs
+++ Assets/Scripts/MyType.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class MyType : MonoBehaviour
+public class MyType
 {
     public enum Unit { Sword, Range, Guard, Wizard, Bullet };
 }
